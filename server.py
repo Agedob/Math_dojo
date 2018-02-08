cebe10cfab20eb427220ab112222f9440686da77
agedob <agedob888@gmail.com>
2018-02-07 19:28:15 -0800

    last commit

--- server.py
+++ server.py
@@ -3,18 +3,33 @@ class MathDojo(object):
         self.var = 0
     def add(self, *arg):
         for i in arg:
-            self.var += i
+            print i,
+        if type(i) == list or type(i) == tuple:
+            for j in i:
+                self.var += j
+                print self.var
+        elif type(i) == int:
+            for k in arg:
+                self.var += i
+                print self.var
         return self
     def subtract(self, *arg):
-        temp = 0
+        temp = 0 
         for i in arg:
-            temp += i
-        self.var -= temp
+            print i,
+        if type(i) == list or type(i) == tuple:
+            for j in i:
+                self.var -= j
+                print self.var
+        elif type(i) == int:
+            for k in arg:
+                self.var -= i
+                print self.var
         return self
     def p(self):
-        print self.var
+        print self.var,'<00000000000000000000'
         return self
 
 
 md = MathDojo()
-md.add(2).add(2,5).subtract(3,2).p()
+md.add([1,2,3,4],10,10).subtract([10,5],5,(10)).p()
